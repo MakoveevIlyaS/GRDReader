28b7527ac11510e96e0725c7fc282e5915438b38
Ilya Makoveev <tyzzz.notes@mail.ru>
2020-06-11 19:51:11 +0400

    1

---
+++ GRDReader.py
@@ -0,0 +1,295 @@
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import struct
+import sys
+import multiprocessing
+import time
+import numpy as np
+from colour import Color
+
+from reader import read_and_draw
+from MapView import MapView
+from AreaList import AreaList
+from AreaAddPanel import AreaAddPanel
+
+class GRDReader(QMainWindow):
+    
+    points = []
+    pointsCounter = 0
+    areasCounter = 0
+    areaWidgets = []
+
+    def __init__(self, *args, **kwargs):
+        super(GRDReader, self).__init__(*args, **kwargs)
+        self.setWindowTitle("grd")
+
+        self.setMenu()
+
+        self.map = MapView(self)
+        self.map.photoClicked.connect(self.addPoint)
+
+        #Main
+        self.layoutMain = QGridLayout()
+
+        #Instruments panel
+        self.instrumentPanel = QWidget()
+        self.instrumentPanelLayout = QHBoxLayout()
+        self.instrumentPanelLayout.setContentsMargins(0, 2, 0, 2)
+        self.toggleDragMode = QPushButton("Прокрутка/выбор точек")
+        self.toggleDragMode.clicked.connect(self.map.toggleDragMode)
+        self.instrumentPanelLayout.addWidget(self.toggleDragMode)
+        self.instrumentPanelLayout.addItem(QSpacerItem(20, 40, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.instrumentPanel.setLayout(self.instrumentPanelLayout)
+
+        #Right panel
+        self.rightPanel = QWidget()
+        self.rightPanel.setFixedWidth(350)
+        self.rightPanelLayout = QBoxLayout(QBoxLayout.TopToBottom)
+        self.rightPanelLayout.setSpacing(0)
+
+        #Widget for adding areas
+        self.areaAddPanel = AreaAddPanel()
+
+        #Add new area button
+        self.addAreaButton = QPushButton("Добавить зону")
+        self.addAreaButton.clicked.connect(self.addArea)
+
+        #List of areas
+        self.areaList = AreaList()
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.layoutMain.addWidget(self.instrumentPanel, 0, 0, 1, 2)
+        self.layoutMain.addWidget(self.map, 1, 0)
+        self.rightPanelLayout.addWidget(self.areaAddPanel)
+        self.rightPanelLayout.addWidget(self.addAreaButton)
+        self.rightPanelLayout.addWidget(self.areaList)
+        self.rightPanelLayout.addItem(self.verticalSpacer)
+        self.rightPanel.setLayout(self.rightPanelLayout)
+        self.layoutMain.addWidget(self.rightPanel, 1, 1)
+
+        mw = QWidget()
+        mw.setLayout(self.layoutMain)
+        self.setGeometry(500, 300, 800, 600)
+        self.setCentralWidget(mw)
+
+    #Creating menu
+    def setMenu(self):
+        #Open .grd
+        openGRDAction = QAction("Открыть .grd файл", self)
+        openGRDAction.setStatusTip('Открыть .grd файл')
+        openGRDAction.triggered.connect(self.openFileDialog)
+
+        openGRDAction2 = QAction("Открыть H.grd файл", self)
+        openGRDAction2.setStatusTip('Открыть .grd файл')
+        openGRDAction2.triggered.connect(self.openFileDialog_H)
+
+        openGRDAction3 = QAction("Delete", self)
+        openGRDAction3.setStatusTip('d')
+        openGRDAction3.triggered.connect(self.Delete)
+
+        openGRDAction4 = QAction("Return", self)
+        openGRDAction4.setStatusTip('d')
+        openGRDAction4.triggered.connect(self.Return)
+
+        #Export
+        exportAction = QAction("Экспорт", self)
+        exportAction.setStatusTip('Экспортировать зоны')
+        exportAction.triggered.connect(self.exportAreas)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        menu = mainMenu.addMenu('&Меню')
+        menu.addAction(openGRDAction)
+        menu.addAction(openGRDAction2)
+        menu.addAction(openGRDAction3)
+        menu.addAction(openGRDAction4)
+        menu.addAction(exportAction)
+    
+    def Delete(self):
+        self.map.Remove()
+        
+    def Return(self):
+        self.map.Return()
+
+    def openFileDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Открыть файл', '/home')[0]
+        if not fname:
+            return 0
+        elif not fname.endswith(".grd"):
+            self.openMesBox("Ошибка!", "Программа принимает только файлы формата .grd")
+            return 0
+
+        self.matrix, self.img = self.readGRD(fname, 30)
+        self.map.setPhoto(self.img)
+
+    def openFileDialog_H(self):
+        fname = QFileDialog.getOpenFileName(self, 'Открыть файл', '/home')[0]
+        if not fname:
+            return 0
+        elif not fname.endswith(".grd"):
+            self.openMesBox("Ошибка!", "Программа принимает только файлы формата .grd")
+            return 0
+
+        self.matrix, self.img_h = self.readGRD_H(fname, 30)
+        #self.map.setPhoto_h(self.img_h)
+        self.map.setPhoto_new(self.img_h)
+   
+       #Add matrix of Relief 
+
+
+    def readGRD(self, filename, levels):
+        green = Color("green")
+        red = Color("#EE1212")
+        #white = Color("#00FFFF")
+        #blue = Color("blue")
+        
+        #print(red.get_hex())
+        colorRange = list(green.range_to(red, levels))
+
+        colorRangeCopy = []
+        colorZeroAlpha = []
+
+        for c in colorRange:
+            col = QColor(c.get_hex())
+            colorRangeCopy.append([col.blue(), col.green(), col.red(), col.alpha()])
+        colorRange = colorRangeCopy
+
+        colorZeroAlpha = [0,0,0,0]
+ 
+
+        matrix, imgMatrix = read_and_draw([filename, colorRange, colorZeroAlpha])
+
+        img = QImage(len(imgMatrix[0]), len(imgMatrix), 5)
+        ptr = img.bits()
+        ptr.setsize(img.byteCount())
+        arr = np.asarray(ptr).reshape(img.height(), img.width(), 4)
+        np.copyto(arr, imgMatrix, casting="unsafe")
+
+        #_pixmap =  QPixmap(QPixmap.fromImage(img))
+
+        return matrix, QPixmap(QPixmap.fromImage(img))
+
+
+    #Add matrix of Water (H_)
+
+    def readGRD_H(self, filename, levels):
+        
+        white = Color("#00FFFF")
+        blue = Color("blue")
+
+        colorRange = list(white.range_to(blue, levels))
+        colorRangeCopy = []
+        colorZeroAlpha = []
+
+        for c in colorRange:
+            col = QColor(c.get_hex())
+            colorRangeCopy.append([col.blue(), col.green(), col.red(), col.alpha()])
+        colorRange = colorRangeCopy
+
+        colorZeroAlpha = [0,0,0,0]
+
+
+        matrix, imgMatrix = read_and_draw([filename, colorRange, colorZeroAlpha])
+
+        img = QImage(len(imgMatrix[0]), len(imgMatrix), 5)
+        ptr = img.bits()
+        ptr.setsize(img.byteCount())
+        arr = np.asarray(ptr).reshape(img.height(), img.width(), 4)
+        np.copyto(arr, imgMatrix, casting="unsafe")
+
+        _pixmap =  QPixmap(QPixmap.fromImage(img))
+
+        return matrix, _pixmap # QPixmap(QPixmap.fromImage(img))
+
+    #Add new point
+    def addPoint(self, event):
+        if(not self.map.isDragMode):
+            x, y = event.x()+1, len(self.matrix) - event.y() + 1
+            point = self.areaAddPanel.addPoint(x, y)
+
+            if(point == 0):
+                self.openMesBox("Ошибка!", "Не удалось построить зону. Выберите другую точку")
+            elif(point):
+                self.points.append(self.map.drawPoint(event.x(), event.y()))
+            else:
+                self.openMesBox("Ошибка!", "Нельзя указать больше 2-х точек")
+
+    #Remove last point
+    def removeLastPoint(self):
+        removedPoint = self.areaAddPanel.removeLastPoint()
+
+        if(removedPoint):
+            self.map._scene.removeItem(self.points.pop())
+
+    #Add area with parameters to list
+    def addArea(self):
+        #text += "({};{})|".format(point[0], point[1])
+
+        if(len(self.points) == 2):
+            self.areaList.addArea(self.areaAddPanel.points[0], 
+                                  self.areaAddPanel.points[1], 
+                                  self.areaAddPanel.angle)
+
+            self.removeLastPoint()
+            self.removeLastPoint()
+            self.areaAddPanel.angleLabel.setText("Угол: ")
+            self.areaAddPanel.angle = ""
+
+    #Message box
+    def openMesBox(self, title, text):
+        dlg = QDialog(self)
+        dlg.setWindowTitle(title)
+        l = QHBoxLayout()
+        label = QLabel()
+        label.setText(text)
+        l.addWidget(label)
+        dlg.setLayout(l)
+        dlg.exec_()
+
+    #Export areas
+    def exportAreas(self):
+        fname = QFileDialog.getSaveFileName(self, 'Экспорт', '/home', "*.dat")[0]
+        file = open(fname, "w")
+        areas = self.areaList.areas
+
+        for i in range(len(areas)):
+            area = areas[i]
+            file.write(str(area.angle)+"\n")
+            
+            if(area.points[0][0] >= area.points[1][0]):
+                file.write(str(area.points[1][0])+"\n")
+                file.write(str(area.points[0][0])+"\n")
+            else:
+                file.write(str(area.points[0][0])+"\n")
+                file.write(str(area.points[1][0])+"\n")
+
+            if(area.points[0][1] >= area.points[1][1]):
+                file.write(str(area.points[1][1])+"\n")
+                file.write(str(area.points[0][1]))
+            else:
+                file.write(str(area.points[0][1])+"\n")
+                file.write(str(area.points[1][1]))
+
+            if(i < len(areas) - 1):
+                file.write("\n")
+
+        file.close()
+
+    #Check shortcuts
+    def keyPressEvent(self, event):
+        
+        #Remove last point
+        if int(event.modifiers()) == (Qt.ControlModifier) and event.key() == Qt.Key_Z:
+            self.removeLastPoint()
+
+app = QApplication(sys.argv)
+
+window = GRDReader()
+window.show()
+
+# Start the event loop.
+app.exec_()
---
+++ Reader/Main.cpp
@@ -0,0 +1,182 @@
+#include <Windows.h>
+#include <Python.h>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+//#include <numpy/arrayobject.h>
+
+using namespace std;
+
+PyObject* read_and_draw(PyObject*, PyObject* args) {
+	const char* filename = PyUnicode_AsUTF8(PyList_GetItem(args, 0));
+	PyObject* colorList = PyList_GetItem(args, 1);
+	
+	PyObject* colorZeroAlpha = PyList_GetItem(args, 2);
+	
+	int levels = PyList_Size(colorList);
+
+	FILE *file;
+	fopen_s(&file, filename, "rb");
+	char* var = new char[4];
+	short Nx, Ny;
+	double Xmin, Xmax, Ymin, Ymax, Zmin, Zmax;
+	float z = 0;
+
+	//read header
+	fread(var, sizeof(char), 4, file);
+	
+	cout << var << endl;
+
+	fread(&Nx, sizeof(short), 1, file);
+
+	cout << Nx << endl;
+
+	fread(&Ny, sizeof(short), 1, file);
+
+
+	fread(&Xmin, sizeof(double), 1, file); fread(&Xmax, sizeof(double), 1, file);
+	fread(&Ymin, sizeof(double), 1, file); fread(&Ymax, sizeof(double), 1, file);
+	fread(&Zmin, sizeof(double), 1, file); fread(&Zmax, sizeof(double), 1, file);
+
+	cout << Zmin << " " << Zmax << endl;
+
+	float** zMatrix = new float* [Ny];
+
+	PyObject* image = PyList_New(Ny);
+	PyObject* res = PyList_New(2);
+	PyObject* matrix = PyList_New(Ny);
+
+	/*float h = (Zmax - Zmin) / levels;
+
+	float* zLevels = new float[levels];
+
+	for (int i = 1; i < levels + 1; i++) {
+		zLevels[i - 1] = Zmin + ((float)h * i);
+	}*/
+
+	/*for (int y = Ny-1; y >= 0; y--) {
+		PyObject* imageRow = PyList_New(Nx);
+		PyObject* matrixRow = PyList_New(Nx);
+
+		for (int x = 0; x < Nx; x++) {
+			fread(&z, sizeof(float), 1, file);
+			PyList_SetItem(matrixRow, x, PyFloat_FromDouble((double)z));
+
+			for (int level = 0; level < levels; level++) {
+				if (z <= zLevels[level]) {
+					PyList_SetItem(imageRow, x, PyList_GetItem(colorList, level));
+					break;
+				}
+			}
+		}
+
+		PyList_SetItem(matrix, y, matrixRow);
+		PyList_SetItem(image, y, imageRow);
+	}*/
+
+	float percent = 0.01 * (Zmax - Zmin);
+	float tZmin = Zmin, 
+		  tZmax = Zmax-(percent);
+
+	float t = Zmin;
+	Zmin = Zmax;
+	Zmax = t;
+
+	for (int y = Ny - 1; y >= 0; y--) {
+		zMatrix[y] = new float[Nx];
+		PyObject* matrixRow = PyList_New(Nx);
+
+		for (int x = 0; x < Nx; x++) {
+			fread(&z, sizeof(float), 1, file);
+			PyList_SetItem(matrixRow, x, PyFloat_FromDouble((double) z));
+			zMatrix[y][x] = z;
+
+			if ((z < Zmin) && (z >= tZmin)) {
+				Zmin = z;
+			}
+			else if ((z > Zmax) && (z <= tZmax)) {
+				Zmax = z;
+			}
+		}
+
+		PyList_SetItem(matrix, y, matrixRow);
+	}
+
+	cout << Zmin << " " << Zmax << endl;
+	fclose(file);
+
+	for (int y = 0; y < Ny; y++) {
+		for (int x = 0; x < Nx; x++) {
+			if (zMatrix[y][x] > Zmax) {
+				zMatrix[y][x] = Zmax;
+			}
+			else if (zMatrix[y][x] < Zmin) {
+				zMatrix[y][x] = Zmin;
+			}
+		}
+	}
+
+	float h = (Zmax - Zmin) / levels;
+
+	float* zLevels = new float[levels];
+
+	for (int i = 1; i < levels + 1; i++) {
+		zLevels[i - 1] = Zmin + ((float)h * i);
+	}
+
+	for (int y = 0; y < Ny; y++) {
+		PyObject* imageRow = PyList_New(Nx);
+
+		for (int x = 0; x < Nx; x++) {
+
+			 //if (round(zMatrix[y][x] * 100) / 100 < 0.001) { PyList_SetItem(imageRow, x, colorZeroAlpha);  }
+
+			if (zMatrix[y][x] <= Zmin ) { PyList_SetItem(imageRow, x, colorZeroAlpha);  }
+			else
+			{
+				for (int level = 0; level < levels; level++) {
+					if (zMatrix[y][x] <= zLevels[level])
+					{
+						PyList_SetItem(imageRow, x, PyList_GetItem(colorList, level));
+						break;
+					}
+				}
+			}
+		}
+
+		PyList_SetItem(image, y, imageRow);
+	}
+
+	for (int y = 0; y < Ny; y++) {
+		delete[] zMatrix[y];
+ 	}
+	
+	delete[] zMatrix;
+
+	PyList_SetItem(res, 0, matrix);
+	PyList_SetItem(res, 1, image);
+	return res;
+}
+
+PyObject* numpy_test(PyObject*, PyObject* args) {
+	PyObject* p = PyList_New(4);
+	return p;
+}
+
+static PyMethodDef reader_methods[] = {
+	{ "read_and_draw", (PyCFunction)read_and_draw, METH_O, nullptr },
+	{ "numpy_test", (PyCFunction)numpy_test, METH_O, nullptr },
+	{ nullptr, nullptr, 0, nullptr }
+};
+
+static PyModuleDef reader_module = {
+	PyModuleDef_HEAD_INIT,
+	"reader",                        
+	"Provides fast function for read and build contours from .grd files",
+	0,
+	reader_methods
+};
+
+PyMODINIT_FUNC PyInit_reader() {
+	return PyModule_Create(&reader_module);
+}
